7298303f37816eac3fbd7e712e2cfbbabc1b9e1e
Nevena Verbic <nevena.verbic@gmail.com>
2023-12-04 22:56:51 +0100

    Add index_all(). It that takes a list of objects and the item to search for as input arguments and returns a list of indices for where that item exists within the list

--- src/04 Find All List Items/index_all.py
+++ src/04 Find All List Items/index_all.py
@@ -1,16 +1,33 @@
 '''Find all items in a multidimensional list
 
-Output:
-Before sort: banana ORANGE apple Green
-After sort: apple banana Green ORANGE
-Before sort: Order this sentence using SORTED function
-After sort: function Order sentence SORTED this using'''
+List example:  example = [[[[1, 2, 3], 2,], [1, 3, 2]], [1, 2, 3]]
 
-def index_all(list_of_objects, item):
-    ''' Find all the instances of the item '''
-    return 0
+Output:
+Found the following occurances of the item : 2 in the multudim. array: 
+ [[0, 0, 0, 1], [0, 0, 1], [0, 1, 2], [1, 1]]'''
+
+def index_all(list_of_objects, item, item_indicies, indices=None):
+    ''' Find all the instances of the item in the list_of_objects'''
+    if indices is None:
+        indices = []
+
+    for i, element in enumerate(list_of_objects):
+        current_indices = indices + [i]
+
+        if isinstance(element, list):
+            # Recursively search nested lists
+            result = index_all(element, item, item_indicies, current_indices)
+            if result is not None:
+                item_indicies.append(result)
+        elif element == item:
+            # Element found, return the indices
+            return current_indices
+    return None
 
 
 if __name__== '__main__':
-    example = [[[1, 2, 3], 2, [1, 3]], [1, 2, 3]]
-    index_all(example, 2)
+    example = [[[[1, 2, 3], 2,], [1, 3, 2]], [1, 2, 3]]
+    item_indicies = []
+    index_all(example, 2, item_indicies)
+    print(f"Found the following occurrences of the item : 2 in the multudim. array: \n {item_indicies}")
+
