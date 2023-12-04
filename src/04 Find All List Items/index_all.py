e94d261f66e60d9c82aedbd07b03e5aa29b05e2b
Nevena Verbic <nevena.verbic@gmail.com>
2023-12-04 21:03:49 +0100

    Add the #4 challenge README

---
+++ src/04 Find All List Items/index_all.py
@@ -0,0 +1,16 @@
+'''Find all items in a multidimensional list
+
+Output:
+Before sort: banana ORANGE apple Green
+After sort: apple banana Green ORANGE
+Before sort: Order this sentence using SORTED function
+After sort: function Order sentence SORTED this using'''
+
+def index_all(list_of_objects, item):
+    ''' Find all the instances of the item '''
+    return 0
+
+
+if __name__== '__main__':
+    example = [[[1, 2, 3], 2, [1, 3]], [1, 2, 3]]
+    index_all(example, 2)
