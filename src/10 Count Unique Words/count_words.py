079723da654bd5176d12d7bc98094da64d5ef056
Nevena Verbic <nevena.verbic@gmail.com>
2023-12-11 20:15:10 +0100

    Solution for #10 LinkedIn Learning Python Code Challenge - completed
    Updated count_word() function to use regex to search for the words
    containing letters, numbers, apostrophes and hyphens.
    
    String is converted to lowercase to ignore case when comparing the
    words and counting the number of the same words used in the file.

--- src/10 Count Unique Words/count_words.py
+++ src/10 Count Unique Words/count_words.py
@@ -1,40 +1,42 @@
 ''' Count the number of words in the file
 
 Output:
-Total number of words used in the text: 961948
+Total number of words used in the text: 976836
 
 The TOP 20 words:
-the : 25536
-I : 20681
-and : 19852
-to : 17016
-of : 16830
-a : 13701
-my : 11421
-in : 10759
-you : 9585
-is : 8334
-that : 8155
-And : 7858
-not : 7410
-with : 7388
-his : 6852
-be : 6398
-your : 6394
-for : 6009
-have : 5588
-it : 5252'''
+the : 30257
+and : 28413
+i : 23070
+to : 20997
+of : 18824
+a : 16163
+you : 14570
+my : 13179
+in : 12333
+that : 12063
+is : 9858
+not : 9066
+with : 8531
+me : 8262
+for : 8244
+it : 8212
+his : 7583
+be : 7384
+this : 7165
+he : 7100'''
 
 
 from collections import Counter
+import re
 
 def count_word(file_path):
     ''' Count the number of words in a text file '''
     with open(file_path, encoding='utf-8') as fp:
         data = fp.read()
-        words = data.split()
+        # Words contain letters, numbers, apostrophes and hyphens
+        # String is converted to lowercase
+        words = re.findall(r"([A-Z0-9'-]+)", data.lower(), re.IGNORECASE)
         print(f"Total number of words used in the text: {len(words)}")
-
         # Count the number of times a specific word occurs
         count = Counter(words)
 
@@ -42,7 +44,6 @@ def count_word(file_path):
     print("\nThe TOP 20 words:")
     for key, value in count.most_common(20):
         print(f"{key} : {value}")
-
 
 if __name__ == '__main__':
     file_path = "src/10 Count Unique Words/shakespeare.txt"
